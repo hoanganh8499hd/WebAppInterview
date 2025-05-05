a6dd807e3ca58c665f83602dc17eca7f60b87901
hoanganh8499hd <hoanganhpham.rasta@gmail.com>
2025-05-05 16:45:59 +0700

    Data Passing Techniques

---
+++ WebAppDataPassing/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebAppDataPassing.Models;
+
+namespace WebAppDataPassing.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    //ViewData là một dictionary (ViewDataDictionary)
+    //cho phép lưu trữ dữ liệu dưới dạng key-value, và dữ liệu được lưu dưới dạng object.
+    public ViewResult ViewDataAction()
+    {
+        //String string Data
+        ViewData["Title"] = "Student Details Page";
+        ViewData["Header"] = "Student Details";
+        Student student = new Student()
+        {
+            StudentId = 101,
+            Name = "James",
+            Branch = "CSE",
+            Section = "A",
+            Gender = "Male"
+        };
+        //storing Student Data
+        ViewData["Student"] = student;
+        return View();
+    }
+
+    public ActionResult ViewDataRedirectToAbout()
+    {
+        ViewData["Message"] = "Đây là thông điệp trước khi redirect.";
+        return RedirectToAction("About");
+    }
+
+    public ActionResult About()
+    {
+        //ViewData["Message"] = "About";
+
+        // Tại đây ViewData["Message"] sẽ là null, vì nó không tồn tại sau redirect.
+        return View();
+    }
+
+    //Sử dụng dynamic để lưu trữ dữ liệu
+    public ActionResult ViewBagAction()
+    {
+        ViewBag.Title = "Student Details Page";
+        ViewBag.Header = "Student Details";
+        Student student = new Student()
+        {
+            StudentId = 101,
+            Name = "James",
+            Branch = "CSE",
+            Section = "A",
+            Gender = "Male"
+        };
+        ViewBag.Student = student;
+        //return View();
+
+        return RedirectToAction("About");
+    }
+}
---
+++ WebAppDataPassing/Controllers/TempDataController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using WebAppDataPassing.Models;
+
+namespace WebAppDataPassing.Controllers
+{
+    public class TempDataController : Controller
+    {
+        public ActionResult Index()
+        {
+            TempData["Name"] = "Pranaya";
+            TempData["Age"] = 30;
+
+
+            //Retention of All keys of TempData for the next request
+            //TempData.Keep();
+            //Retention of Individual keys of TempData for the next request
+            TempData.Keep("Name");
+            TempData.Keep("Age");
+
+            //Create the Complex Object
+            var student = new Student()
+            {
+                StudentId = 1,
+                Name = "Pranaya",
+                Gender = "Male",
+                Branch = "CSE",
+                Section = "A"
+            };
+            //Convert the Complex Object to Json
+            string jsonStudent = JsonSerializer.Serialize(student);
+            //Store the JSON Objec into the TempData
+            TempData["StudentObject"] = jsonStudent;
+            //return RedirectToAction("Privacy", "Home");
+            return RedirectToAction("About");
+
+
+            //return View();
+        }
+        public ActionResult Privacy()
+        {
+
+            //Retention of Individual keys of TempData for the next request
+            if (TempData.ContainsKey("Name"))
+            {
+                //Peek Method will read the data and preserve the key for next request
+                ViewData["Name"] = TempData.Peek("Name");
+            }
+            if (TempData.ContainsKey("Age"))
+            {
+                //Peek Method will read the data and preserve the key for next request
+                ViewData["Age"] = TempData.Peek("Age");
+            }
+
+
+            ////Retention of Individual keys of TempData for the next request
+            //TempData.Keep("Name");
+            //TempData.Keep("Age");
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            //Retention of Individual keys of TempData for the next request
+            //TempData.Keep("Name");
+            //TempData.Keep("Age");
+            Student? student = new Student();
+            if (TempData["StudentObject"] is string jsonStudent)
+            {
+                //Deserialize the Json Object to Actual Student Object
+                student = JsonSerializer.Deserialize<Student>(jsonStudent);
+                //You can use the Student
+                // The following line keeps the data for another request
+
+            }
+            return View(student);
+
+            //return View();
+        }
+    }
+}
